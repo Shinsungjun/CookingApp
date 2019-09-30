213a90718c1b88a9c2df5d572eeb64237e1f8291
holy_joon <ssjun511@gmail.com>
2019-09-30 23:14:19 +0900

    popup 완성 .. popup 손보는중

--- app/src/main/java/kau/holyjoon/cookingapp/EditActivity.kt
+++ app/src/main/java/kau/holyjoon/cookingapp/EditActivity.kt
@@ -4,6 +4,10 @@ package kau.holyjoon.cookingapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.edit_main.*
 
 
@@ -22,7 +26,16 @@ class EditActivity : AppCompatActivity() {
         }
     }
     private fun openPlusActivity() {
-        val startIntent = Intent(this, PlusActivity::class.java)
-        startActivity(startIntent)
+        val builder = AlertDialog.Builder(this)
+        val dialogView = layoutInflater.inflate(R.layout.plus_popup,null)
+        val dialogText = dialogView.findViewById<EditText>(R.id.edit_how)
+
+        builder.setView(dialogView).show()
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
--- app/src/main/java/kau/holyjoon/cookingapp/PlusActivity.kt
+++ app/src/main/java/kau/holyjoon/cookingapp/PlusActivity.kt
@@ -1,11 +1,21 @@
 package kau.holyjoon.cookingapp
 
 import android.os.Bundle
+import android.view.Window
+import android.widget.BaseAdapter
+import android.widget.GridView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.plus_popup.*
 
 class PlusActivity :AppCompatActivity() {   //+ 버튼 클릭 시 나타나는 popup형식 Activity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.plus_popup)
+
+        val gridLayoutManager = GridLayoutManager(this,5)
+
+        val gridView : GridView = findViewById(R.id.grid_image)
     }
 }
